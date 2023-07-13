9dea6ab94d87b775f628caa63bd1e1d26fc41bf2
hassan <Hassang15g>
2023-07-13 23:53:54 +0300

    for while

--- loops.py
+++ loops.py
@@ -2,8 +2,65 @@
 # loops ###############
 # while loop 
 
-x = 0 
-while x < 10 :
-    x +=1
-    print (x)
+# x = 0 
+# while x < 10 :
+#     x +=1
+#     print (x)
+# else :
+#     print ("x <= 10")
 
+# Nested while
+# x = 0 
+# while x < 5 :
+#     y = 0 
+#     while y < 3 :
+#         print(x ,y)
+#         y +=1
+#     x+=1    
+
+
+# #########################
+# for Loops
+# ##########
+
+# for i in "python" :
+#     print(i)
+
+# for i in [1 ,3 ,3 ,4 ,5 , "fffffffffffffff" ] :
+#     print (i)
+
+# for i in range(4):
+#     print("-----")
+#     for y in "hassan-":
+#         print(i  , y)
+
+# امسله  while
+
+# i = 0
+# x = 0
+# while i < 4 :
+#     i += 1 
+#     x = 0
+#     while x < 4 :
+#         x+=1
+#         print(i , x)
+
+# امسله  for 
+
+# for i in range(0,5) :
+#     print("---")
+#     for x in range(1,5):
+#         print(i ,x)
+
+
+# ###################### Range
+#  Range  شرح
+# print (list(range(20)))
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+# print (list(range( 5 , 20 )))
+# [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+                    #    العدد من كل مره بي ,     العدد المطلوب  , البديه
+# print (list(range(    10      ,      50               ,     2    ) ))
+# [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48]
