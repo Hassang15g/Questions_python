1d2e25b7f59bf526ce33898fe6d4b32c0511b1cb
hassan <Hassang15g>
2023-07-13 20:26:52 +0300

    loops

---
+++ if_conditions.py
@@ -0,0 +1,127 @@
+# Help Method 
+
+# myName = []
+
+# help(myName)
+
+# ########################################################################
+# if 
+
+# Conditions
+# if , elif , else
+
+# x = 4
+
+# if x == 5 :
+#     print(f"Hello {x}")
+# elif x !=5 :
+#     print("ffffffff")
+
+
+# name = "hassan"
+# password = "2222"
+
+# if name =="hassan" and password =="22122":
+#     print ("hello")
+# elif name == "Ail" or password == "2222":
+#     print ("No name and erorr")
+# else :
+#     print ("dddddddddddd")
+    
+
+
+# x = 500
+
+# if x >= 600 :
+#     print("gooo")
+# elif x != 500 :
+#     print("hhhh")
+# else :
+#     print("ffff")
+
+# x = 1504
+
+# if x == 200 :
+#     print (f"in x ==200 {x}")
+# elif x == 100 :
+#     print (f"in x ==100 {x}")
+# elif x == 150 :
+#     print (f" in x == 150 {x}")
+
+# else :
+#     print ("No end x ")
+
+
+
+# x = 200
+
+# if x < 300 :
+#     print ("No x end 200 ")
+#     if x == 150 :
+#         print ("x == 150")
+#     if x == 200 :
+#         print ('x ==200')
+# else :
+#     print ("no fun")
+
+# Questions on if  (all , any)
+# x = 5 
+# y = 4
+# c = 3
+# f = 7
+
+# if all ([x == 5 , y == 4 , c == 3 , f == 7 ]):
+#     print ("good all")
+# if any ([x == 4 , y == 2 , c == 3 , f == 1]) :
+#     print ("No arorr")
+
+
+
+# x = {"vr " :1  , "vn" :2 }
+
+# if "vne" in x :
+#     print("good")
+# else:
+#     print("no")    
+
+# dav  = {"fr": 2 , "fv": 3}
+
+# if "fr" in dav :
+#     print("hhhh")
+
+# x = 1
+# e = 2
+
+# if x == 1 and  e == 2 :
+#     print(True)
+# if x == 3 or e == 2 :
+#     print (True)
+# if x == 1 and e == 4 :
+#     print(True)
+# if x != 0 and e != 5 :
+#     print(True)
+# if not (x == 3 and e == 2):
+#     print (True)
+
+
+# number = 200
+
+# if not number ==300 :
+#     print (True)
+
+# name = {"hassan" : 2 , "ali" : 1}
+
+# if "hassan" not in name :
+#     print( "No ooooo")
+# elif "hassan" in name :
+#     print("True")
+
+
+
+# x = 1 
+
+# if x in (1 , 3 , 2):
+#     print ( "Match") كاده هيكون النتج   (Match)
+# else :
+#     print("No Match")
+
