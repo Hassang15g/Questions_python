cd5e9194cca1208c2e23b0d63a04a92a4b54faad
hassan <Hassang15g>
2023-07-31 00:19:09 +0300

    cfcf

---
+++ oop/Oriented.py
@@ -0,0 +1,12 @@
+class student:
+    def __init__(self , name):
+        self.name = name 
+        self.marks = []
+
+        print(f"wicome {name} to the schoot ")
+
+    def addMarks (self , marks):
+        self.marks.append(mark)
+    
+x= student()
+x.name= "hassan"
